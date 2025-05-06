4fc157e4d684ece04fa3e4b2c3aca6357835c21c
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-06 22:00:13 +0900

    1.1.2が完了。

--- Chapter1/main/main1.py
+++ Chapter1/main/main1.py
@@ -1,5 +1,7 @@
 from PIL import Image
 
-im1=Image.open('main/photo/IMG_6417.jpg').convert('L')
+im1=Image.open('main/photo/IMG_6417.jpg')
 im1g=im1.convert('L')
 im1g.save('out/IMG_6417.png')
+im1g.thumbnail((128,128))
+im1g.save('out/IMG_6417(2).png')
