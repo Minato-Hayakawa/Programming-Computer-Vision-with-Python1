060d6ba2e8d91af1fb292ac9efb3ab011c893bcc
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-07 13:36:13 +0900

    imtoolsを追加

--- Chapter1/main/imtools.py
+++ Chapter1/main/imtools.py
@@ -1,11 +1,16 @@
 from PIL import Image
 import numpy as num
-def imresize(im,sz):
-    pil_im=Image.fromarray(uint8(im))
-    return num.array(pil_im.resize(sz))
-def histeq(im,nbr_bins=256):
-    imhist,bins=num.histogram(im.flatten(),nbr_bins,normed=True)
-    cdf=imhist.cumsum()
-    cdf=255*cdf/cdf[-1]
-    im2=num.interp(im.flatten(),bins[:-1],cdf)
-    return im2.reshape(im.shape),cdf
+class imtools:
+    def imresize(im,sz):
+        pil_im=Image.fromarray(uint8(im))
+        return num.array(pil_im.resize(sz))
+    def histeq(im,nbr_bins=256):
+        imhist,bins=num.histogram(im.flatten(),nbr_bins,normed=True)
+        cdf=imhist.cumsum()
+        cdf=255*cdf/cdf[-1]
+        im2=num.interp(im.flatten(),bins[:-1],cdf)
+        return im2.reshape(im.shape),cdf
+
+imtoolsobj=imtools()
+im1=num.array(Image.open('out/IMG_6417.jpg'))
+im2,cdf=imtoolsobj.
