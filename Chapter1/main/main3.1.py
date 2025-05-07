88a182e9d2552ad233d530f20b5cec269673727b
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-07 13:58:12 +0900

    main3.1.pyを修正しました。

--- Chapter1/main/main3.1.py
+++ Chapter1/main/main3.1.py
@@ -10,8 +10,5 @@ im3=255-im2 #画像を反転
 im4=(100.0/255)*im1+100
 im5=255.0*(im1/255.0)**2
 show()
-def imresize(im,sz):
-    pil_im=Image.fromarray(uint8(im))
-    return array(pil_im.resize(sz))
 
 print(im2.shape ,im2.dtype)
