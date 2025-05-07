10cfd76f42f325005465aaa411b33b85af5193d5
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-07 09:40:48 +0900

    1.2.1が完了。

---
+++ Chapter1/main/main2.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from pylab import *
+
+im1 = array(Image.open('out/IMG_6417.jpg'))
+imshow(im1)
+x=[50,40,30,20]
+y=[40,40,30,40]
+
+plot(x,y,'r*')
+plot(x[:2],y[:2])
+
+title('plotting:"ebara.jpg"')
+show()
