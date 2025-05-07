341f8dd0f80f83e3aa060e05786b8a9b211546a4
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-07 09:59:40 +0900

    1.3.1が完了。

--- Chapter1/main/main3.1.py
+++ Chapter1/main/main3.1.py
@@ -4,5 +4,6 @@ im1=num.array(Image.open('out/IMG_6417.jpg'))
 print (im1.shape ,im1.dtype)
 
 im2=num.array(Image.open('out/IMG_6417.jpg').convert('L'),'f')
+#'f'でfloatにしている。
 
 print(im2.shape ,im2.dtype)
