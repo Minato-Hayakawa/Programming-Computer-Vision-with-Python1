53152b9f0f3b8c0519d8352503c8866728045430
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-05-07 10:06:03 +0900

    1.3.2が完了。

--- Chapter1/main/main3.1.py
+++ Chapter1/main/main3.1.py
@@ -1,9 +1,14 @@
 from PIL import Image
 import numpy as num
+from pylab import *
 im1=num.array(Image.open('out/IMG_6417.jpg'))
 print (im1.shape ,im1.dtype)
 
 im2=num.array(Image.open('out/IMG_6417.jpg').convert('L'),'f')
 #'f'でfloatにしている。
+im3=255-im2 #画像を反転
+im4=(100.0/255)*im1+100
+im5=255.0*(im1/255.0)**2
+show()
 
 print(im2.shape ,im2.dtype)
